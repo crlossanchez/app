5953adfe70313a994eae4d24c0695cceb6fc392a
crlossanchez <juan.c.sanchez.c@gmail.com>
2017-05-01 21:58:16 -0400

    first commit

---
+++ src/pages/inventario/inventario.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-inventario',
+  templateUrl: 'inventario.html'
+})
+export class InventarioPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
